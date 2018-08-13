05892dab730bf191d8cab5aa63d4eec066a72832
lishaokai <1113923376@qq.com>
2018-08-13 20:12:57 +0800

    第一次提交

---
+++ t1.py
@@ -0,0 +1,118 @@
+# -*- encoding:utf-8 -*-
+#print('李少凯')
+'''
+msg1 = '老男孩教育'
+msg2 = '是最好的教育机构'
+print(msg1 + msg2)
+
+msg3 = 'LOL'
+print(msg3*8)
+
+print(5>10)
+print(True)
+print('True',type('True'))
+
+name=input('请输入你的名字')
+age=input('请输入你的年龄')
+print(name,age,type(name),type(age))
+
+name = input('请输入你的名字')
+if name == '王爷':
+	print('老铁，没毛病')
+else:
+	print('有病得治......')
+
+num = int(input('请输入你的选择'))
+if
+
+count = 1
+while count < 101:
+	print(count)
+	count=count+1
+'''
+'''
+count1 = 1
+count2 = 0
+while True:
+	count2 = count1 + count2
+	count1 = count1 + 1
+	if count1 == 101:
+		break
+print(count2)
+
+# -*- encoding:utf-8 -*-
+
+print('中国')
+age_of_oldboy=56
+number_of_students=80
+
+age1 = 12
+age2 = age1
+age3 = age2
+age2 = 23
+print(age1,age2,age3)	
+
+print('这不是字符串')
+print("这不是字符串")
+'''
+msg = '''
+床前明月光
+疑是地上霜
+举头望明月
+低头思故乡
+'''
+#msg1 = '老男孩教育'
+#msg2 = '是最好的教育机构'
+#print(msg)
+
+#i = '大山'
+#print(i*8)
+
+
+#print('True',type('True'))
+#print(True,type(True))
+
+#name = input('请输入你的名字')
+#age = input('请输入你的年龄')
+#print(name,age)
+
+#print(111)
+#if 3 > 2:
+ #   print(666)
+#print(333)
+'''
+name =input('请输入你的名字')
+if name=='时光':
+	print('老铁，没毛病')
+else:
+	print('你有病，但是我没药')
+
+num = int(input('输入你的选择'))
+if num == 4:
+	print('早饭我请了')
+elif num == 5:
+	print ('午饭我请了')
+else :
+	print('给你机会抓不住')
+
+num1 = input('请输入数字')
+if num1 == '3':
+	num2 = input('请输入数字')
+	if num2 == '5':
+		print('这都能猜对')
+	else:
+		print('继续努力')
+'''
+flag = True
+while flag:
+	print('牵丝戏')
+	print('凉凉')
+	flag = False
+	print('刚好遇见你')
+
+
+
+
+
+
+
